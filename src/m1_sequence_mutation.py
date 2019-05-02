39bd15f49119033d44a52f4b1cec7d05ed6f9270
saylorsa <saylorsa@rose-hulman.edu>
2019-05-02 10:17:54 -0400

    m1, Done - 3

--- src/m1_sequence_mutation.py
+++ src/m1_sequence_mutation.py
@@ -82,7 +82,7 @@ def zero_changer(tuple_of_lists):
       :type tuple_of_lists: tuple of list[int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
