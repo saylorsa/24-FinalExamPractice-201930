d3dbceb8bb35e0f4a9a442ee2103d13f1f80c892
saylorsa <saylorsa@rose-hulman.edu>
2019-05-02 10:49:37 -0400

    m3, Done - 3

--- src/m3_loops.py
+++ src/m3_loops.py
@@ -225,7 +225,7 @@ def practice_problem3(start, n, threshold):
       :type threshold: float
     """
     ###########################################################################
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above),
     #          but you are required to write ADDITIONAL tests (above).
     ###########################################################################
@@ -233,6 +233,17 @@ def practice_problem3(start, n, threshold):
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   < 15 minutes.
     ###########################################################################
+    list = []
+    count = start
+    while True:
+        if len(list) < n:
+            total = math.sin(count) + math.cos(count)
+            if total > threshold:
+                list.append(count)
+            count = count +1
+        else:
+            break
+    return list
 
 
 # -----------------------------------------------------------------------------
